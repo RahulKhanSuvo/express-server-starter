07a5e6c2c947a7372a818dccdbe2acd2f345a1d1
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-01 19:05:03 +0600

    feat: initialize Better Auth configuration with Prisma adapter and email/password authentication

---
+++ src/app/lib/auth.ts
@@ -0,0 +1,12 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "../../generated/prisma/client";
+
+export const auth = betterAuth({
+  database: prismaAdapter(PrismaClient, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+});
