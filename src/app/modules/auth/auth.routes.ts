6f42c043b8db44144259a6cb66a77e9bf2f98136
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-01 19:36:16 +0600

    feat: implement authentication routes for sign-in, sign-up, and token refresh

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 
 const router = Router();
 
-router.post("/signin");
+router.post("/sign-in");
+router.post("/sign-up");
+router.post("/refresh-token");
 
 export const authRoutes = router;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import { authRoutes } from "../modules/auth/auth.routes";
 import { SpecialtyRoutes } from "../modules/specialty/specialty.routes";
 
 const router = express.Router();
 
+router.use("/auth", authRoutes);
 router.use("/specialty", SpecialtyRoutes);
 
 export const AppRoutes = router;
