43c5190f57fa28d2910a7b42932f1811698c8b71
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-02 10:18:43 +0600

    refactor: update catchAsync error type from any to unknown for better type safety

--- src/shared/catchAsync.ts
+++ src/shared/catchAsync.ts
@@ -1,12 +1,11 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, RequestHandler, Response } from "express";
 
 export const catchAsync = (fn: RequestHandler) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       await fn(req, res, next);
-    } catch (error: any) {
-      next(error);
+    } catch (error: unknown) {
+      next(error as Error);
     }
   };
 };
