d1e91097538016feee3245431da3d9efa98fed60
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-02 08:43:38 +0600

    refactor: disable unused variable linting and comment out next function call in notFound middleware

--- src/app/middleware/notFound.ts
+++ src/app/middleware/notFound.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express";
 import { status } from "http-status";
 
@@ -10,5 +11,5 @@ export const notFoundHandler = (
     success: false,
     message: `${req.originalUrl} Not Found`,
   });
-  next();
+  // next();
 };
