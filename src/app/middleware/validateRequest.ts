8688abda3abb40cbb4aece446ae917add7aa2b8b
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-02 08:39:58 +0600

    refactor: format zod validation error messages into a key-value object map

--- src/app/middleware/validateRequest.ts
+++ src/app/middleware/validateRequest.ts
@@ -6,10 +6,16 @@ export const validateRequest = (schema: z.ZodObject) => {
   return (req: Request, res: Response, next: NextFunction) => {
     const result = schema.safeParse(req.body);
     if (!result.success) {
+      const formattedErrors: Record<string, string> = {};
+
+      result.error.issues.forEach((issue) => {
+        const key = issue.path.join(".");
+        formattedErrors[key] = issue.message;
+      });
       return res.status(status.BAD_REQUEST).json({
         success: false,
         message: "Validation failed",
-        error: result.error.issues,
+        error: formattedErrors,
       });
     }
     req.body = result.data;
