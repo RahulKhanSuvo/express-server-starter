1f05b7da478872295da15baa7beba9b14c2753f1
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-04 20:47:01 +0600

    feat: restrict specialty routes to admin users using AuthGard middleware

--- src/app/modules/specialty/specialty.routes.ts
+++ src/app/modules/specialty/specialty.routes.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { SpecialtyController } from "./specialty.controller";
+import { AuthGard } from "../../middleware/authGard";
+import { Role } from "../../../generated/prisma/enums";
 
 const router = express.Router();
 
-router.post("/", SpecialtyController.createSpecialty);
-router.get("/", SpecialtyController.getAllSpecialty);
+router.post("/", AuthGard(Role.ADMIN), SpecialtyController.createSpecialty);
+router.get("/", AuthGard(Role.ADMIN), SpecialtyController.getAllSpecialty);
 
 export const SpecialtyRoutes = router;
