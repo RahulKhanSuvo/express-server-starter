ad528c91964d6626b6e8ed9911439cd8f66a1f41
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-05 23:37:51 +0600

    feat: protect doctor detail route with role-based authentication guard

--- src/app/modules/doctor/doctor.routes.ts
+++ src/app/modules/doctor/doctor.routes.ts
@@ -1,7 +1,13 @@
 import { Router } from "express";
 import { doctorController } from "./doctor.controller";
+import { AuthGard } from "../../middleware/authGard";
+import { Role } from "../../../generated/prisma/enums";
 
 const routes = Router();
 routes.get("/", doctorController.getAllDoctors);
-routes.get("/:id", doctorController.getDoctorById);
+routes.get(
+  "/:id",
+  AuthGard(Role.ADMIN, Role.DOCTOR, Role.PATIENT),
+  doctorController.getDoctorById,
+);
 export const DoctorRoutes = routes;
