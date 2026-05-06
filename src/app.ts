5611e40a5e3463ec19a8a4bf1e02502e99ea9ba2
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-06 11:33:23 +0600

    refactor: replace process.env references with imported envConfig in app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,18 @@ import globalErrorHandler from "./app/middleware/globalErrorHandler";
 import morgan from "morgan";
 import { notFoundHandler } from "./app/middleware/notFound";
 import cookieParser from "cookie-parser";
+import envConfig from "./config/env";
 const app: Application = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+if (envConfig.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 app.get("/", (req: Request, res: Response) => {
   res.json({
     message: "Hello World",
-    environment: process.env.NODE_ENV,
+    environment: envConfig.NODE_ENV,
   });
 });
 app.use("/api/v1", AppRoutes);
