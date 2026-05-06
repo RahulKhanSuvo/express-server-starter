e8a85414e7c432768ec22de10ef704ccc9bc71ce
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-06 18:08:37 +0600

    feat: implement dynamic EJS email templating and OTP template support in email utility

--- src/app/utils/email.ts
+++ src/app/utils/email.ts
@@ -1,5 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import nodemailer from "nodemailer";
 import envConfig from "../../config/env";
+import AppError from "../errorsHelpers/AppError";
+import status from "http-status";
+import path from "path";
+import ejs from "ejs";
 
 const transporter = nodemailer.createTransport({
   host: envConfig.EMAIL_SENDER_SMTP_HOST,
@@ -10,15 +15,49 @@ const transporter = nodemailer.createTransport({
     pass: envConfig.EMAIL_SENDER_SMTP_PASS,
   },
 });
-
-const sendEmail = async (to: string, subject: string, html: string) => {
-  const mailOptions = {
-    from: envConfig.EMAIL_SENDER_SMTP_FROM,
-    to,
-    subject,
-    html,
-  };
-  await transporter.sendMail(mailOptions);
+interface sendEmailType {
+  to: string;
+  subject: string;
+  templateName: string;
+  templateData: Record<string, any>;
+  attachments?: {
+    filename: string;
+    content: Buffer | string;
+    contentType: string;
+  }[];
+}
+const sendEmail = async ({
+  to,
+  subject,
+  templateName,
+  templateData,
+  attachments,
+}: sendEmailType) => {
+  try {
+    const templatePath = path.resolve(
+      process.cwd(),
+      `src/app/templates/${templateName}.ejs`,
+    );
+    const html = await ejs.renderFile(templatePath, templateData);
+    const info = await transporter.sendMail({
+      from: envConfig.EMAIL_SENDER_SMTP_FROM,
+      to,
+      subject,
+      html,
+      attachments: attachments?.map((attachment) => ({
+        filename: attachment.filename,
+        content: attachment.content,
+        contentType: attachment.contentType,
+      })),
+    });
+    return info;
+  } catch (error) {
+    console.error("❌ Error while sending email", error);
+    throw new AppError(
+      status.INTERNAL_SERVER_ERROR,
+      "❌ Error while sending email",
+    );
+  }
 };
 export const EmailUtils = {
   sendEmail,
