60724ad2d0c31ef7754f872f728875425986ffa7
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-12 17:19:46 +0600

    feat: add multer file upload middleware to specialty creation route

--- src/app/modules/specialty/specialty.routes.ts
+++ src/app/modules/specialty/specialty.routes.ts
@@ -2,10 +2,16 @@ import express from "express";
 import { SpecialtyController } from "./specialty.controller";
 import { AuthGard } from "../../middleware/authGard";
 import { Role } from "../../../generated/prisma/enums";
+import { multerUpload } from "../../../config/multer.config";
 
 const router = express.Router();
 
-router.post("/", AuthGard(Role.ADMIN), SpecialtyController.createSpecialty);
+router.post(
+  "/",
+  AuthGard(Role.ADMIN),
+  multerUpload.single("file"),
+  SpecialtyController.createSpecialty,
+);
 router.get("/", AuthGard(Role.ADMIN), SpecialtyController.getAllSpecialty);
 
 export const SpecialtyRoutes = router;
