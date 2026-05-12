7e6dc00e2bb5df26623e96588f2b7fb3952294c4
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-12 17:14:38 +0600

    feat: configure multer storage with Cloudinary for file uploads and dynamic folder categorization

---
+++ src/config/multer.config.ts
@@ -0,0 +1,29 @@
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { cloudinaryUpload } from "./cloudinary.config";
+import multer from "multer";
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinaryUpload,
+  params: async (req, file) => {
+    const originalName = file.originalname;
+    const fileExtension = originalName.split(".").pop()?.toLocaleLowerCase();
+    const fileWithoutExtension = originalName
+      .split(".")
+      .slice(0, -1)
+      .join(".")
+      .toLocaleLowerCase()
+      .replace(/\s/g, "_")
+      .replace(/[^A-Za-z0-9_\-.]/g, "");
+    const uniqueName =
+      Math.random().toString(36).substring(2, 10) +
+      "-" +
+      Date.now() +
+      "-" +
+      fileWithoutExtension;
+    const folder = fileExtension === "pdf" ? "Reports" : "Images";
+    return {
+      folder: `healthCare/${folder}`,
+      public_id: `${fileWithoutExtension}-${uniqueName}.${fileExtension}`,
+    };
+  },
+});
+export const multerUpload = multer({ storage });
