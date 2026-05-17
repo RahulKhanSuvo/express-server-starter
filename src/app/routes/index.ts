14f67020d753f9c5e57bb7c0daa0038b2a34d4c5
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-17 16:49:08 +0600

    feat: add routes for doctor schedule management

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,6 +4,7 @@ import { SpecialtyRoutes } from "../modules/specialty/specialty.routes";
 import { UserRoutes } from "../modules/user/user.routes";
 import { DoctorRoutes } from "../modules/doctor/doctor.routes";
 import { ScheduleRoutes } from "../modules/schedule/schedule.route";
+import { DoctorScheduleRoutes } from "../modules/doctorSchedule/doctorSchedule.route";
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.use("/specialty", SpecialtyRoutes);
 router.use("/user", UserRoutes);
 router.use("/doctors", DoctorRoutes);
 router.use("/schedule", ScheduleRoutes);
+router.use("/doctor-schedule", DoctorScheduleRoutes);
 
 export const AppRoutes = router;
