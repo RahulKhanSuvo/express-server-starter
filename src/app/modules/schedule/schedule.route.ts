9f85d41152b849509ef309d681ff1f5644c3e830
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-17 11:28:17 +0600

    feat: add request validation schema to schedule creation route

--- src/app/modules/schedule/schedule.route.ts
+++ src/app/modules/schedule/schedule.route.ts
@@ -2,10 +2,13 @@ import { Router } from "express";
 import { ScheduleController } from "./schedule.controller";
 import { AuthGard } from "../../middleware/authGard";
 import { Role } from "../../../generated/prisma/enums";
+import { ScheduleInterface } from "./schedule.interface";
+import { validateRequest } from "../../middleware/validateRequest";
 
 const routes = Router();
 routes.post(
   "/",
+  validateRequest(ScheduleInterface.createScheduleSchema),
   AuthGard(Role.ADMIN, Role.DOCTOR, Role.PATIENT),
   ScheduleController.getAllSchedule,
 );
