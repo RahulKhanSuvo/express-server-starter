4b3d9f3c6c325aa2c06db03d526af28cbd04e130
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-18 11:40:51 +0600

    feat: add updatePatientData validation schema for comprehensive patient health records

--- src/app/modules/patient/patient.validation.ts
+++ src/app/modules/patient/patient.validation.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { BloodGroup, Gender } from "../../../generated/prisma/enums";
 
 const updatePatient = z.object({
   name: z.string().optional(),
@@ -7,7 +8,35 @@ const updatePatient = z.object({
   contactNumber: z.string().optional(),
   address: z.string().optional(),
 });
+const updatePatientData = z.object({
+  gender: z.enum([Gender.MALE, Gender.FEMALE, Gender.OTHER]).optional(),
+  dateOfBirth: z.date(),
+  bloodGroup: z.enum([
+    BloodGroup.AB_NEGATIVE,
+    BloodGroup.AB_POSITIVE,
+    BloodGroup.B_NEGATIVE,
+    BloodGroup.B_POSITIVE,
+    BloodGroup.O_NEGATIVE,
+    BloodGroup.O_POSITIVE,
+    BloodGroup.A_NEGATIVE,
+    BloodGroup.A_POSITIVE,
+  ]),
+  hasAllergies: z.boolean().default(false),
+  hasDiabetes: z.boolean().default(false),
+  height: z.string(),
+  weight: z.string(),
+  smokingStatus: z.boolean().default(false),
+  dietaryPreferences: z.string().optional(),
+  pregnancyStatus: z.boolean().default(false),
+  mentalHealthHistory: z.string().optional(),
+  immunizationStatus: z.string().optional(),
+  hasPastSurgeries: z.boolean().default(false),
+  recentAnxiety: z.boolean().default(false),
+  recentDepression: z.boolean().default(false),
+  maritalStatus: z.string().optional(),
+});
 
 export const PatientValidation = {
   updatePatient,
+  updatePatientData,
 };
